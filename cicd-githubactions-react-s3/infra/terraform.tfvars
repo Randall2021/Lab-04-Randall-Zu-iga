820286bda0fc9e63cd27541488030f5026ba46d7
Randall2021 <84815924+Randall2021@users.noreply.github.com>
2023-11-06 18:42:24 -0600

    Add files via upload

---
+++ cicd-githubactions-react-s3/infra/terraform.tfvars
@@ -0,0 +1,5 @@
+bucket_name = "821533968716-randallbucket"
+
+common_tags = {
+    Project = "Game App"
+}
---
+++ cicd-githubactions-react-s3/infra/variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+    type = string
+    description = "The name of the bucket"
+  
+}
+
+variable "common_tags" {
+    description = "Common tags you can applied to all components"
+}
